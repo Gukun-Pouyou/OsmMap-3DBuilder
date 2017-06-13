e44211cff33e1ba9c50a85230f09e9b8609ad30d
Gukun-Pouyou <xa4bureau@gmail.com>
2017-06-13 11:07:05 +0200

    colors & three.js
    colors contains information about the materials to apply
    three.js is the reknown library

---
+++ colors.js
@@ -0,0 +1,31 @@
+var colors = {};
+
+colors.background = 0xcccccc;
+colors.building = {
+	diffuse : 0x999999,
+	ambient : 0x888888
+}
+colors.road = {
+	diffuse : 0x000000,
+	ambient : 0x888899
+}
+
+colors.water = {
+	diffuse : 0x000000,
+	ambient : 0x00a5cd
+}
+
+colors.forest = {
+	diffuse : 0x000000,
+	ambient : 0x007700
+}
+
+colors.grass = {
+	diffuse : 0x000000,
+	ambient : 0x45ba6f
+}
+
+colors.field = {
+	diffuse : 0x000000,
+	ambient : 0xabb417
+}
